9144e63b292c43f2ad0bb0f01711f30f83b2aea9
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-07-30 00:42:53 -0500

    :boom: Added additional OpenAPI swagger config

---
+++ ManageTasks.API/Installers/SwaggerInstaller.cs
@@ -0,0 +1,24 @@
+using ManageTasks.API.Options;
+
+namespace ManageTasks.API.Installers
+{
+  public class SwaggerInstaller : IInstaller
+  {
+    public void InstallServices(IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
+    {
+      IConfigurationSection swaggerSection = configuration.GetSection(nameof(SwaggerOptions));
+      services.Configure<SwaggerOptions>(swaggerSection);
+      SwaggerOptions swagger = swaggerSection.Get<SwaggerOptions>();
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1", new()
+        {
+          Title = "Manage Tasks API",
+          Version = "v1",
+          Description = "Backend project with an API to manage tasks",
+          Contact = swagger.Contact
+        });
+      });
+    }
+  }
+}
